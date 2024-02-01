620bf8a4d022c5d18d79d3b44debb4eefb463763
rasel <jucse28.367@gmail.com>
2024-02-01 10:14:22 +0600

    updated offline browsing

--- lib/models/home_model.dart
+++ lib/models/home_model.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:repository_explorer/constants/strings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../services/github_api.dart';
 
 class HomeModel extends ChangeNotifier {
@@ -18,11 +20,11 @@ class HomeModel extends ChangeNotifier {
   String get sortOrder => _sortOrder;
 
   HomeModel() {
-    performSearch(true);
+    initialize();
   }
 
   Future<void> initialize() async {
-    performSearch(true);
+    await performSearch(true);
   }
 
   void updateKeyword(String newKeyword) {
@@ -34,7 +36,7 @@ class HomeModel extends ChangeNotifier {
 
   void updateSortOrder(String newSortOrder) {
     _sortOrder = newSortOrder;
-     _currentPage = 1;
+    _currentPage = 1;
     performSearch(true);
     notifyListeners();
   }
@@ -43,19 +45,40 @@ class HomeModel extends ChangeNotifier {
     if (_isLoading) {
       return;
     }
+
     try {
+      final connectivityResult = await Connectivity().checkConnectivity();
+
       if (clear) _searchResults.clear();
       notifyListeners();
       _isLoading = true;
 
-      final results = await GitHubApiService()
-          .searchRepositories(_keyword, _currentPage, _sortOrder);
+      // Check connectivity status
+      if (connectivityResult != ConnectivityResult.none) {
+        // Fetch data from the network
+        final results = await GitHubApiService()
+            .searchRepositories(_keyword, _currentPage, _sortOrder);
 
-      final List<Map<String, dynamic>> repositories =
-          List.castFrom<dynamic, Map<String, dynamic>>(results);
+        final List<Map<String, dynamic>> repositories =
+            List.castFrom<dynamic, Map<String, dynamic>>(results);
 
-      _searchResults.addAll(repositories);
-      _currentPage++;
+        _searchResults.addAll(repositories);
+        _currentPage++;
+
+        // Cache the results for offline browsing
+        await cacheResults(json.encode(_searchResults));
+      } else {
+        // If offline, check for cached data
+        final cachedResults = await getCachedResults();
+        if (cachedResults != null) {
+          _searchResults.addAll(
+            cachedResults.cast<Map<String, dynamic>>(),
+          ); // Explicit casting
+        } else {
+          // Handle no cached data for offline mode
+          print('No cached data available for offline mode');
+        }
+      }
     } catch (e) {
       if (kDebugMode) {
         print('Error during search: $e');
@@ -67,10 +90,38 @@ class HomeModel extends ChangeNotifier {
     }
   }
 
-  String formatDateTime(String timestamp) {
-  final DateFormat dateFormat = DateFormat('MMMM dd, yyyy hh:mm a', 'en_US');
-  final DateTime dateTime = DateTime.parse(timestamp);
-  return dateFormat.format(dateTime.toLocal());
-}
+  Future<List<dynamic>?> getCachedResults() async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? cachedResults = prefs.getString('cachedResults');
 
+      if (cachedResults != null) {
+        final List<dynamic> results = json.decode(cachedResults);
+        return results;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error fetching cached results: $e');
+      }
+    }
+    return null;
+  }
+
+  Future<void> cacheResults(String results) async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('cachedResults', results);
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error caching results: $e');
+      }
+    }
+  }
+
+  ///Formating Last updated date
+  String formatDateTime(String timestamp) {
+    final DateFormat dateFormat = DateFormat('MMMM dd, yyyy hh:mm a', 'en_US');
+    final DateTime dateTime = DateTime.parse(timestamp);
+    return dateFormat.format(dateTime.toLocal());
+  }
 }
--- lib/services/github_api.dart
+++ lib/services/github_api.dart
@@ -1,21 +1,71 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GitHubApiService {
   static const String baseUrl = 'https://api.github.com';
   static const int perPage = 10;
 
-  Future<List<dynamic>> searchRepositories(String keyword, int page,String sortOrder) async {
+  Future<List<dynamic>> searchRepositories(String keyword, int page, String sortOrder) async {
+    // Checkinh if cached data is available
+    final cachedData = await _getCachedData(keyword, page, sortOrder);
+    if (cachedData != null) {
+      return cachedData;
+    }
+
+    // Fetching data from the API
     final response = await http.get(
-      Uri.parse( '$baseUrl/search/repositories?q=$keyword&page=$page&per_page=$perPage&sort=$sortOrder'),
-       headers: {'Accept': 'application/vnd.github+json'},
+      Uri.parse('$baseUrl/search/repositories?q=$keyword&page=$page&per_page=$perPage&sort=$sortOrder'),
+      headers: {'Accept': 'application/vnd.github+json'},
     );
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
+
+      // storing  the fetched data
+      _cacheData(keyword, page, sortOrder, data['items']);
+
       return data['items'];
     } else {
       throw Exception('Failed to load repositories');
     }
+  }
+
+  Future<void> _cacheData(String keyword, int page, String sortOrder, List<dynamic> data) async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = _generateCacheKey(keyword, page, sortOrder);
+    prefs.setString(key, json.encode(data));
+  }
+
+ Future<List<dynamic>?> _getCachedData(String keyword, int page, String sortOrder) async {
+  try {
+    final prefs = await SharedPreferences.getInstance();
+    final key = _generateCacheKey(keyword, page, sortOrder);
+    final cachedData = prefs.getString(key);
+
+    if (cachedData != null) {
+      List<dynamic> results = json.decode(cachedData);
+
+      // Sorting the cached data based on the provided sortOrder
+      if (sortOrder == 'stars') {
+        results.sort((a, b) => b['stargazers_count'].compareTo(a['stargazers_count']));
+      } else if (sortOrder == 'updated') {
+        results.sort((a, b) => DateTime.parse(b['updated_at']).compareTo(DateTime.parse(a['updated_at'])));
+      }
+
+      return results;
+    }
+  } catch (e) {
+    if (kDebugMode) {
+      print('Error fetching and sorting cached results: $e');
+    }
+  }
+  return null;
+}
+
+
+  String _generateCacheKey(String keyword, int page, String sortOrder) {
+    return '$keyword:$page:$sortOrder';
   }
 }
